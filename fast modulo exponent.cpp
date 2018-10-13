28ecceccc8502ae53172d1a0b6f1dced48a8bb81
BarjinderPaul <barjinderpaul.singh@gmail.com>
2018-10-13 12:00:34 +0530

    Exponent of  a number with modulo.

---
+++ fast modulo exponent.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+uint64_t p=1000000007;
+uint64_t calcExpo(uint64_t base, uint64_t expo){
+	if(expo == 0)
+		return 1;
+	if(expo == 1)
+		return base % p;
+	uint64_t t=1;
+	while(expo > 0){
+		if(expo%2!=0)
+			t = (t*base) % p;
+		base = (base*base)%p;
+		expo /= 2; 
+		
+	}
+	return t%p;
+}
+
+int main(){
+	uint64_t base,expo;
+	cin>>base>>expo;
+	cout<<calcExpo(base,expo);
+}
